46c3d6e33ebbca2c6114f43b79d76058bbff633a
San Neupane <123here.comes.san@gmail.com>
2019-10-18 11:29:10 +0545

    changing code syntax and making easy

--- Trees and Graphs/Priority_Prims.cpp
+++ Trees and Graphs/Priority_Prims.cpp
@@ -1,56 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <functional>
-#include <utility>
+/*
+    Time  : 2019-10-18,Friday
+    Desc  : no description
+*/
+
+#include <bits/stdc++.h>
+
+#define endl                         '\n'
+#define ALLONS_Y                     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define pii                          pair<int,int>
 
 using namespace std;
-const int MAX = 1e4 + 5;
-typedef pair<long long, int> PII;
-bool marked[MAX];
-vector <PII> adj[MAX];
 
-long long prim(int x)
-{
-    priority_queue<PII, vector<PII>, greater<PII> > Q;
-    int y;
-    long long minimumCost = 0;
-    PII p;
-    Q.push(make_pair(0, x));
-    while(!Q.empty())
-    {
-        // Select the edge with minimum weight
-        p = Q.top();
-        Q.pop();
-        x = p.second;
-        // Checking for cycle
-        if(marked[x] == true)
+int prims(vector<pii> g[],int v){
+
+    priority_queue<pii, vector<pii>,greater<pii> > pq;
+    vector<bool> visited(v,false);
+
+    int minCost = 0;
+    
+    pq.push({0,0});
+
+    while(!pq.empty()){
+        pii top = pq.top();
+        pq.pop();
+        
+        if(visited[top.second]){
             continue;
-        minimumCost += p.first;
-        marked[x] = true;
-        for(int i = 0;i < adj[x].size();++i)
-        {
-            y = adj[x][i].second;
-            if(marked[y] == false)
-                Q.push(adj[x][i]);
+        }
+
+        minCost += top.first;
+        visited[top.second] = true;
+
+        for(auto it : g[top.second]){
+            if(!visited[it.second]){
+                pq.push(it);
+            }
         }
     }
-    return minimumCost;
+
+    return minCost;
 }
 
-int main()
-{
-    int nodes, edges, x, y;
-    long long weight, minimumCost;
-    cin >> nodes >> edges;
-    for(int i = 0;i < edges;++i)
-    {
-        cin >> x >> y >> weight;
-        adj[x].push_back(make_pair(weight, y));
-        adj[y].push_back(make_pair(weight, x));
+int main() {
+    ALLONS_Y;
+
+    int v,e;
+    cin >> v >> e; 
+    vector<pii> g[v];
+
+    int src,dest,wt;
+    for(int i = 0; i < e; i++){
+        cin >> src >> dest >> wt;
+        g[src].push_back({wt,dest});
+        g[dest].push_back({wt,src});
     }
-    // Selecting 1 as the starting node
-    minimumCost = prim(1);
-    cout << minimumCost << endl;
+
+    cout << prims(g,v) << endl;
+
     return 0;
 }
